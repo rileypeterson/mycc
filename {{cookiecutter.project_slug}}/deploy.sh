30278d60490d4213e799ec16340fcc987e945a44
Riley Peterson <rileypeterson21@gmail.com>
2023-02-17 23:38:34 -0800

    Change env file path

--- {{cookiecutter.project_slug}}/deploy.sh
+++ {{cookiecutter.project_slug}}/deploy.sh
@@ -29,7 +29,7 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker compose --env-file ./config/.prod.env -f docker-compose.prod.yml run --rm --entrypoint "\
+docker compose --env-file ./config/.prod.env -f docker/docker-compose.prod.yml run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
@@ -40,7 +40,7 @@ echo "### Starting containers ..."
 prod-up-detach
 
 echo "### Deleting dummy certificate for $domains ..."
-docker compose --env-file ./config/.prod.env -f docker-compose.prod.yml run --rm --entrypoint "\
+docker compose --env-file ./config/.prod.env -f docker/docker-compose.prod.yml run --rm --entrypoint "\
   rm -Rf /etc/letsencrypt/live/$domains && \
   rm -Rf /etc/letsencrypt/archive/$domains && \
   rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
@@ -62,7 +62,7 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker compose --env-file ./config/.prod.env -f docker-compose.prod.yml run --rm --entrypoint "\
+docker compose --env-file ./config/.prod.env -f docker/docker-compose.prod.yml run --rm --entrypoint "\
   certbot certonly --webroot -w /var/www/certbot \
     $staging_arg \
     $email_arg \
