7077dcea590210831ce7f6e1f175b093cb8d1410
Riley Peterson <rileypeterson21@gmail.com>
2023-02-18 10:46:35 -0800

    Only self signed

--- {{cookiecutter.project_slug}}/deploy.sh
+++ {{cookiecutter.project_slug}}/deploy.sh
@@ -39,41 +39,41 @@ echo
 echo "### Starting containers ..."
 prod-up-detach
 
-echo "### Deleting dummy certificate for $domains ..."
-docker compose --env-file ./config/.prod.env -f docker/docker-compose.prod.yml run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
-
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
-
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
-
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
-
-docker compose --env-file ./config/.prod.env -f docker/docker-compose.prod.yml run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
-    $staging_arg \
-    $email_arg \
-    $domain_args \
-    --rsa-key-size $rsa_key_size \
-    --agree-tos \
-    --no-eff-email \
-    --force-renewal" certbot
-echo
-
-echo "### Re-deploying ..."
-sudo chown -R {{ cookiecutter.linux_user }}:{{ cookiecutter.linux_user }} docker/certbot
-prod-down
-prod-up-detach
+#echo "### Deleting dummy certificate for $domains ..."
+#docker compose --env-file ./config/.prod.env -f docker/docker-compose.prod.yml run --rm --entrypoint "\
+#  rm -Rf /etc/letsencrypt/live/$domains && \
+#  rm -Rf /etc/letsencrypt/archive/$domains && \
+#  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+#echo
+#
+#echo "### Requesting Let's Encrypt certificate for $domains ..."
+##Join $domains to -d args
+#domain_args=""
+#for domain in "${domains[@]}"; do
+#  domain_args="$domain_args -d $domain"
+#done
+#
+## Select appropriate email arg
+#case "$email" in
+#  "") email_arg="--register-unsafely-without-email" ;;
+#  *) email_arg="--email $email" ;;
+#esac
+#
+## Enable staging mode if needed
+#if [ $staging != "0" ]; then staging_arg="--staging"; fi
+#
+#docker compose --env-file ./config/.prod.env -f docker/docker-compose.prod.yml run --rm --entrypoint "\
+#  certbot certonly --webroot -w /var/www/certbot \
+#    $staging_arg \
+#    $email_arg \
+#    $domain_args \
+#    --rsa-key-size $rsa_key_size \
+#    --agree-tos \
+#    --no-eff-email \
+#    --force-renewal" certbot
+#echo
+#
+#echo "### Re-deploying ..."
+#sudo chown -R {{ cookiecutter.linux_user }}:{{ cookiecutter.linux_user }} docker/certbot
+#prod-down
+#prod-up-detach
